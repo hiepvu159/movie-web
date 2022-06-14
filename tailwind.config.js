7ee521316d10ef791fe304b078a3d1d3535194ce
hiepvu159 <vhhiep1509@gmail.com>
2022-06-14 15:09:08 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        logo: ["Chakra Petch", "sans-serif"],
+        sans: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
